bef8c907dd970b144a3d720c3086e61ddc324309
Martin Lank <martas.lank@gmail.com>
2019-04-27 01:07:47 +0200

    make songId param in review PUT route not required, as it is not used at all

--- backend/src/main/kotlin/cz/cvut/fit/vwm/collaborativefiltering/route/ReviewsRoute.kt
+++ backend/src/main/kotlin/cz/cvut/fit/vwm/collaborativefiltering/route/ReviewsRoute.kt
@@ -60,7 +60,7 @@ fun Route.reviews(storage: IDatabaseInteractor) {
         put<ReviewLoc> {
             val user = getLoggedUserOrRespondForbidden(storage) ?: return@put
             val newReview = call.receiveOrNull<Review>()
-            if (newReview == null || newReview.songId == 0) {
+            if (newReview == null) {
                 call.respond(HttpStatusCode.BadRequest)
                 return@put
             }
