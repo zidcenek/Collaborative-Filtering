bb66200023bb168acd1433f01321406608c574db
Martin Lank <martas.lank@gmail.com>
2019-04-27 00:02:39 +0200

    review unique index

--- backend/src/main/kotlin/cz/cvut/fit/vwm/collaborativefiltering/db/dao/Reviews.kt
+++ backend/src/main/kotlin/cz/cvut/fit/vwm/collaborativefiltering/db/dao/Reviews.kt
@@ -4,7 +4,14 @@ import org.jetbrains.squash.definition.*
 
 object Reviews : TableDefinition() {
     val id = integer("id").autoIncrement().primaryKey()
-    val userId = integer("user_id").index()
+    val userId = integer("user_id")
     val songId = integer("song_id")
     val value = integer("value")
+
+    init {
+        constraints.add(IndexConstraint(
+                Identifier("UIX_review_user_song"),
+                listOf(userId, songId),
+                true))
+    }
 }
